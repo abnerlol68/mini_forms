d40ffd2dd8b141d7c8c38447beaf4adbc29d86b7
Abner Perez <abnerstr81@gmail.com>
2022-10-26 00:17:31 -0500

    API (CRUD Forms)

--- src/Libs/Requests.php
+++ src/Libs/Requests.php
@@ -1,12 +1,45 @@
 <?php
   require_once 'src/Libs/Database.php';
-
-  if ($_GET['req'] == 'get_forms') {
+  
+  if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_GET['req'] == 'get_forms') {
     $forms = [...$conn->query('SELECT * FROM forms')];
     echo json_encode($forms);
+    http_response_code(200);
     return;
   }
 
-  echo null;
+  if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_GET['req'] == 'add_form') {
+    $content_req = json_decode(file_get_contents('php://input'), true);
+    $stm = $conn->prepare('INSERT INTO forms VALUES (:id, :title, :author)');
+    $stm->execute([
+      'id' => $content_req['id'],
+      'title' => $content_req['title'],
+      'author' => $content_req['author'],
+    ]);
+    http_response_code(201);
+    return;
+  }
+
+  if ($_SERVER['REQUEST_METHOD'] == 'DELETE' && $_GET['req'] == 'remove_form') {
+    $content_req = json_decode(file_get_contents('php://input'), true);
+    $stm = $conn->prepare('DELETE FROM forms WHERE id_form = :id');
+    $stm->execute([ 'id' => $content_req['id'] ]);
+    http_response_code(200);
+    return;
+  }
+
+  if ($_SERVER['REQUEST_METHOD'] == 'PUT' && $_GET['req'] == 'update_form') {
+    $content_req = json_decode(file_get_contents('php://input'), true);
+    $stm = $conn->prepare('UPDATE forms SET title_form = :title WHERE id_form = :id');
+    $stm->execute([
+      'id' => $content_req['id'],
+      'title' => $content_req['title'],
+    ]);
+    http_response_code(201);
+    return;
+  }
+
+  echo 'Operation not available';
+  http_response_code(400);
   return;
 ?>
