dab1c489cf75f4db866aa001b585aa3b7d802790
Abner Perez <abnerstr81@gmail.com>
2022-10-29 11:55:53 -0500

    Remove dir public

--- src/View/FormBuilder.php
+++ src/View/FormBuilder.php
@@ -11,7 +11,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Builder Form</title>
-  <link rel="stylesheet" href="<?= constant('URL') . 'public/assets/css/form-builder.css' ?>">
+  <link rel="stylesheet" href="<?= URL . 'src/css/form-builder.css' ?>">
   <script src="<?= URL . 'src/App/FormBuilder.js' ?>" type="module"></script>
 </head>
 <body>
