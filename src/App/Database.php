67d5bdbf482a78a4089d1972148420e6db571bd6
JorshBG <jordiyair29@gmail.com>
2022-11-14 20:15:42 -0600

    GUI update

--- src/App/Database.php
+++ src/App/Database.php
@@ -7,17 +7,17 @@ use PDO;
 class Database {
   private string $db;
   private string $host;
-//  private string $port;
+  private string $port;
   private string $user;
   private string $password;
 
-  public function __construct() {
-    $this->db = "miniforms";
-    $this->host = "localhost";
-    $this->port = "3306";
-    $this->user = "root";
-    $this->password = "Alucard29+";
-  }
+    public function __construct() {
+        $this->db       = DB_NAME;
+        $this->host     = DB_HOST;
+        $this->port     = DB_PORT;
+        $this->user     = DB_USER;
+        $this->password = DB_PASS;
+    }
 
   public function get_conn(): PDO {
     return new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->password);
